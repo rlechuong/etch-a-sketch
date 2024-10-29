3543909a3a986907aa3091063c3d8f6acc757923
rlechuong <169632088+rlechuong@users.noreply.github.com>
2024-10-29 04:20:04 -0400

    Initial raw commit with some functionality

---
+++ index.js
@@ -0,0 +1,75 @@
+const createButton = document.querySelector(".createButton");
+createButton.addEventListener("click", createGrid);
+
+const gridContainer = document.querySelector(".gridContainer");
+
+let dimension = 1;
+
+function createGrid() {
+    do { 
+        dimension = prompt("Squares Per Side? (Max = 100)", 10);
+    } while (dimension > 100);
+
+    gridContainer.textContent = "";
+
+    for (i = 0; i < dimension; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("gridRow");
+        gridContainer.appendChild(gridRow);
+
+        for (j = 0; j < dimension; j++) {
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("gridBox");
+
+        let rowNumber = i.toString();
+        let columnNumber = j.toString();
+        let gridID = rowNumber + columnNumber;
+        gridBox.setAttribute("id", parseInt(gridID));
+
+        gridBox.style.opacity=0.1;
+        
+
+        gridBox.addEventListener("mouseover", () => {
+            if (gridBox.style.backgroundColor === "") {
+                gridBox.style.backgroundColor = randomRGB();
+            }
+        });
+
+        gridBox.addEventListener("mouseover", () => {
+            const gridBoxStyle = getComputedStyle(gridBox);
+            let gridBoxOpacity = parseFloat(gridBoxStyle.opacity);
+            console.log(typeof gridBoxOpacity);
+
+            let newGridBoxOpacity = 0;
+
+            if (gridBoxOpacity < 1) {
+                newGridBoxOpacity = gridBoxOpacity + 0.1;
+            }
+            else {
+                newGridBoxOpacity = gridBoxOpacity;
+            }
+
+            console.log(newGridBoxOpacity);
+
+            gridBox.style.opacity = newGridBoxOpacity.toString();
+            console.log(gridBoxOpacity);
+        });
+
+        gridRow.appendChild(gridBox);
+        }
+    }
+}
+
+function randomRGB() {
+    const red = Math.floor(Math.random() * 257);
+    const green = Math.floor(Math.random() * 257);
+    const blue = Math.floor(Math.random() * 257);
+
+    const randomRGB = `rgb(${red}, ${green}, ${blue})`;
+    return randomRGB;
+}
+// let totalBoxes = (dimension * dimension);
+
+// for (i = 0; i < totalBoxes, i++) {
+//     const 
+// }
